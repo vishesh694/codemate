cdd3cef0b0e677531d4437bb6af7d9d51369d04c
vishesh bansal <visheshbansal121@gmail.com>
2026-07-10 17:40:12 +0530

    Add code playground with editor, terminal, templates, and WebContainer support

--- lib/template.ts
+++ lib/template.ts
@@ -1,6 +1,6 @@
 export const templatePaths = {
   REACT: "/vibecode-starters/react-ts",
-  NEXTJS: "/vibecode-starters/nextjs-new",
+  NEXTJS: "/vibecode-starters/nextjs",
   EXPRESS: "/vibecode-starters/express-simple",
   VUE: "/vibecode-starters/vue",
   HONO: "/vibecode-starters/hono-nodejs-starter",
